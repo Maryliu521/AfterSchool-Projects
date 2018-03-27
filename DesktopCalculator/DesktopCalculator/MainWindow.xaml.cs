c7825a891bec267fc81ebc0f0ae4c276b3dbd4e1
MaryLiu <maryrthebest@gmail.com>
2018-03-27 09:34:02 -0700

    update

--- DesktopCalculator/DesktopCalculator/MainWindow.xaml.cs
+++ DesktopCalculator/DesktopCalculator/MainWindow.xaml.cs
@@ -32,95 +32,104 @@ namespace DesktopCalculator
             rt.BeginAnimation(RotateTransform.AngleProperty, da);
         }
 
-        private void zero_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "0";
+        //private void zero_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "0";
 
-        }
+        //}
 
-        private void point_Click(object sender, RoutedEventArgs e)
+        //private void point_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += ".";
+        //}
+
+        //private void one_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "1";
+        //}
+
+        //private void two_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "2";
+        //}
+
+        //private void three_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "3";
+        //}
+
+        //private void four_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "4";
+        //}
+
+        //private void five_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "5";
+        //}
+
+        //private void six_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "6";
+        //}
+
+        //private void seven_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "7";
+        //}
+
+        //private void eight_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "8";
+        //}
+
+        //private void nine_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "9";
+        //}
+
+        //private void par1_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "(";
+        //}
+
+        //private void par2_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += ")";
+        //}
+
+        //private void plus_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "+";
+        //}
+
+        //private void minus_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "-";
+        //}
+
+        //private void mul_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "*";
+        //}
+
+        //private void div_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Screen.Text += "/";
+        //}
+
+            //Instaed using top lumsum code, changing all the "Click" to "Button_Click"
+      
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Screen.Text += ".";
+            Button btn = (Button)sender;
+            Screen.Text += btn.Content.ToString();
+
         }
 
         private void clear_Click(object sender, RoutedEventArgs e)
         {
             Screen.Text = "";
-        }
-
-        private void one_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "1";
-        }
-
-        private void two_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "2";
-        }
-
-        private void three_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "3";
-        }
-
-        private void four_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "4";
-        }
-
-        private void five_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "5";
-        }
-
-        private void six_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "6";
-        }
-
-        private void seven_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "7";
-        }
-
-        private void eight_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "8";
-        }
-
-        private void nine_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "9";
-        }
-
-        private void par1_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "(";
-        }
-
-        private void par2_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += ")";
-        }
-
-        private void plus_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "+";
-        }
-
-        private void minus_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "-";
-        }
-
-        private void mul_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "*";
-        }
-
-        private void div_Click(object sender, RoutedEventArgs e)
-        {
-            Screen.Text += "/";
         }
 
         private void back_Click(object sender, RoutedEventArgs e)
@@ -151,5 +160,6 @@ namespace DesktopCalculator
             }
           
         }
+
     }
 }
